1195e4606d47bcf8fb97eced12ce82e8146d5662
rf464 <rf464@live.mdx.ac.uk>
2021-03-05 13:23:30 +0000

    new commit

--- service-worker1.js
+++ service-worker1.js
@@ -36,7 +36,7 @@ self.addEventListener('fetch', function (e){
     e.respondWith(
         caches.match(e.request).then(function (r){
             console.log('[Service Worker] Fetching resources: ' + e.request.url);
-            return r || fetch(e.request).then(function (cache){
+            return r || fetch(e.request).then(function (response){
                 return caches.open(cacheName).then(function (cache) {
                     cache.put(e.request, response.clone());
                     return response;
